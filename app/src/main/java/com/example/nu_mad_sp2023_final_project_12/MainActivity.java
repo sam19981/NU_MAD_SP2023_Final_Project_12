2c8efd07a9a30c9b0ace28e0d38bdbe4648a9dff
bharath sanjay <bharathsanjay4321@gmail.com>
2023-04-10 22:11:03 -0400

    gradle changes

--- app/src/main/java/com/example/nu_mad_sp2023_final_project_12/MainActivity.java
+++ app/src/main/java/com/example/nu_mad_sp2023_final_project_12/MainActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    FirebaseUser mUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        getSupportFragmentManager().beginTransaction().replace(R.id.rootLayoutId, new LoginFragment(), "maintologin").commit();
-       getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayoutId,new HomeFragment(),"hello").commit();
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        mUser = mAuth.getCurrentUser();
+      getSupportFragmentManager().beginTransaction().replace(R.id.rootLayoutId, new LoginFragment(), "maintologin").commit();
+       //getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayoutId,new HomeFragment(),"hello").commit();
     }
 }
