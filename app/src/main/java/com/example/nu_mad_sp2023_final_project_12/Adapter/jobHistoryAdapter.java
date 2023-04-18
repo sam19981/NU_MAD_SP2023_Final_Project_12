c64c3f848db0daa99daf6747c17fe1f3ec7acaaa
bharath sanjay <bharathsanjay4321@gmail.com>
2023-04-18 18:42:41 -0400

    added status update btn

--- app/src/main/java/com/example/nu_mad_sp2023_final_project_12/Adapter/jobHistoryAdapter.java
+++ app/src/main/java/com/example/nu_mad_sp2023_final_project_12/Adapter/jobHistoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.nu_mad_sp2023_final_project_12.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.nu_mad_sp2023_final_project_12.HistoryDetails;
 import com.example.nu_mad_sp2023_final_project_12.JobDescription;
+import com.example.nu_mad_sp2023_final_project_12.MainActivity;
 import com.example.nu_mad_sp2023_final_project_12.R;
 import com.example.nu_mad_sp2023_final_project_12.models.Jobs;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class jobHistoryAdapter extends RecyclerView.Adapter<jobHistoryAdapter.ViewHolder> {
@@ -28,6 +34,7 @@ public class jobHistoryAdapter extends RecyclerView.Adapter<jobHistoryAdapter.Vi
     }
 
     private Context context;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
 
@@ -50,6 +57,10 @@ public class jobHistoryAdapter extends RecyclerView.Adapter<jobHistoryAdapter.Vi
         private final ImageView j_img;
         private final TextView j_title;
         private final TextView j_status;
+
+
+
+        private final ImageView completed;
         View currentHistory;
 
         public ViewHolder(@NonNull View itemView) {
@@ -57,7 +68,11 @@ public class jobHistoryAdapter extends RecyclerView.Adapter<jobHistoryAdapter.Vi
             j_img = itemView.findViewById(R.id.h_img);
             j_title = itemView.findViewById(R.id.h_name);
             j_status = itemView.findViewById(R.id.h_status);
+            completed = itemView.findViewById(R.id.mark_as_cmp);
             currentHistory = itemView;
+        }
+        public ImageView getCompleted() {
+            return completed;
         }
 
         public ImageView getJ_img() {
@@ -83,15 +98,41 @@ public class jobHistoryAdapter extends RecyclerView.Adapter<jobHistoryAdapter.Vi
                 .into(holder.getJ_img());
         holder.getJ_title().setText(jobs.get(position).getName());
         holder.getJ_status().setText(jobs.get(position).getStatus());
+        if(currJob.getPostedBy().equals(MainActivity.getCurrentBio().getEmail())){
+            holder.getCompleted().setVisibility(View.VISIBLE);
+            holder.getCompleted().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String new_status = "";
 
-        holder.currentHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AppCompatActivity appCompatActivity = (AppCompatActivity)context;
-                appCompatActivity.getSupportFragmentManager()
-                        .beginTransaction().replace(R.id.rootLayoutId, HistoryDetails.newInstance("","",currJob)).addToBackStack(null).commit();
-            }
-        });
+                    if(currJob.getStatus().contains(":")){
+                        String[] arr = currJob.getStatus().split(":");
+                        arr[0] = "completed : ";
+                        new_status = arr[0] + arr[1];
+                    }
+                    else{
+                        new_status = "completed";
+                    }
+                    db.collection("jobs").document(currJob.getJob_id()).update("status",new_status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("completed job","completed");
+                            holder.getCompleted().setVisibility(View.INVISIBLE);
+                        }
+                    });
+
+                }
+            });
+        }
+
+//        holder.currentHistory.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AppCompatActivity appCompatActivity = (AppCompatActivity)context;
+//                appCompatActivity.getSupportFragmentManager()
+//                        .beginTransaction().replace(R.id.rootLayoutId, HistoryDetails.newInstance("","",currJob)).addToBackStack(null).commit();
+//            }
+//        });
 
 
 
